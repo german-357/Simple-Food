2b491ae48357c19a8494d6de0547f3a653a8aee8
German Popov <popov.german6@gmail.com>
2023-06-28 12:17:19 +0200

    HW2

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,37 @@
+
+
+$(function() {
+
+
+  $(".reviews__slider").slick({
+    dots: true,
+    arrows: true,
+    appendDots: $(".reviews__slide-dots"),
+    prevArrow: $(".reviews__arrow--prev"),
+    nextArrow: $(".reviews__arrow--next")
+  });
+
+});
+
+function linkActive() {
+  let links = document.querySelectorAll('.menu__link');
+
+  links.forEach(function(link) {
+    link.addEventListener('click', function() {
+      
+      links.forEach(function(link) {
+        link.classList.remove('menu__link--active');
+      });
+
+      event.target.classList.add('menu__link--active');
+    });
+  });
+}
+
+linkActive(); 
+
+
+
 window.addEventListener('scroll', function() {
   const header = document.querySelector('.header__top');
   if (window.scrollY > 0) {
@@ -7,20 +41,8 @@ window.addEventListener('scroll', function() {
   }
 });
 
-const filterBox = document.querySelectorAll('.categorys__card-item');
-const buttons = document.querySelectorAll('.categorys__button');
 
-buttons.forEach(button => {
-  button.addEventListener('click', event => {
-    console.log('works');
 
-    let filterClass = event.target.dataset['f'];
 
-    filterBox.forEach(elem => {
-      elem.classList.add('hide');
-      if (!elem.classList.contains(filterClass)) {
-        elem.classList.remove('hide');
-      }
-    });
-  });
-});
+
+mixitup('.categories__card-list');
--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ const autoprefixer = require('gulp-autoprefixer');
 const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
 const del = require('del');
+const svgSprite = require('gulp-svg-sprite');
+const cheerio = require('gulp-cheerio');
+const replace = require('gulp-replace');
 const browserSync = require('browser-sync').create();
 
 function browsersync()  {
@@ -31,6 +34,8 @@ function styles() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/mixitup/dist/mixitup.js',
+    'node_modules/slick-carousel/slick/slick.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
@@ -55,6 +60,30 @@ function images() {
   .pipe(dest('dist/images'))
 }
 
+function svgSprites() {
+  return src('app/images/icons/*.svg') 
+  .pipe(cheerio({
+        run: ($) => {
+            $("[fill]").removeAttr("fill"); 
+            $("[stroke]").removeAttr("stroke"); 
+            $("[style]").removeAttr("style"); 
+        },
+        parserOptions: { xmlMode: true },
+      })
+  )
+	.pipe(replace('&gt;','>')) 
+	.pipe(
+	      svgSprite({
+	        mode: {
+	          stack: {
+	            sprite: '../sprite.svg', 
+	          },
+	        },
+	      })
+	    )
+	.pipe(dest('app/images')); 
+}
+
 function build() {
   return src([
     'app/**/*.html',
@@ -72,6 +101,7 @@ function watching() {
   watch(['app/scss/**/*.scss'], styles)
   watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts)
   watch(['app/**/*.html']).on('change', browserSync.reload)
+  watch(['app/images/icons/*.svg'], svgSprites)
 }
 
 
@@ -81,7 +111,8 @@ exports.browsersync = browsersync;
 exports.watching = watching;
 exports.images = images;
 exports.cleanDist = cleanDist;
+exports.svgSprites = svgSprites;
 exports.build = series(cleanDist, images, build);
 
-exports.default = parallel(styles, scripts, browsersync, watching)
+exports.default = parallel(styles, scripts,svgSprites, browsersync, watching)
 
